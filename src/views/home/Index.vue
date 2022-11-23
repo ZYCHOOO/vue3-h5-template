c70aba546f3df039f09fe760695e68e5e510ddc4
曾逸超 <zengyichao@digibms.com>
2022-11-23 09:31:55 +0800

    style：修改样式

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -18,7 +18,7 @@ getUserInfo({ token: 'admin-token' }).then(res => {
 
 <style lang="scss" scoped>
   .index {
-    padding: .1rem;
+    padding: 10px;
     min-height: 100vh;
     background: $main-bg-color;
     overflow: auto;
