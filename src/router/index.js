adafe586a8e260f31a8124870ff3169624ce6024
曾逸超 <zengyichao@digibms.com>
2023-03-02 16:37:46 +0800

    feat: 添加虚拟列表页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,41 +2,59 @@ import store from '@/store'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import demoRoutes from './modules/demo'
 
-const routes = [{
-  path: '/',
-  redirect: '/home'
-}, {
-  path: '/home',
-  name: 'Home',
-  redirect: '/home/index',
-  component: () => import(/* webChunkName: 'hoome' */ '@/views/home/Home'),
-  children: [{
-    path: 'index',
-    name: 'Index',
-    component: () => import(/* webpackChunkName: 'home-index' */ '@/views/home/Index'),
-    meta: { title: '首页', showNavbar: false }
-  }, {
-    path: 'custom',
-    name: 'Custom',
-    component: () => import(/* webpackChunkName: 'home-custom' */ '@/views/home/Custom'),
-    meta: { title: '自定义', showNavbar: false }
-  }, {
-    path: 'mine',
-    name: 'Mine',
-    component: () => import(/* webpackChunkName: 'home-mine' */ '@/views/home/Mine'),
-    meta: { title: '我的', showNavbar: false }
-  }]
-}, {
-  path: '/login',
-  name: 'Login',
-  component: () => import(/* webpackChunkName: "login" */ '@/views/login/Login'),
-  meta: { title: '登录', showNavbar: false },
-  beforeEnter: (to, from, next) => {
-    const { token } = store.state.userModule
-    token ? next('/') : next()
-  }
-},
-...demoRoutes
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    redirect: '/home/index',
+    component: () => import(/* webChunkName: 'hoome' */ '@/views/home/Home'),
+    children: [
+      {
+        path: 'index',
+        name: 'Index',
+        component: () =>
+          import(/* webpackChunkName: 'home-index' */ '@/views/home/Index'),
+        meta: { title: '首页', showNavbar: false }
+      },
+      {
+        path: 'custom',
+        name: 'Custom',
+        component: () =>
+          import(/* webpackChunkName: 'home-custom' */ '@/views/home/Custom'),
+        meta: { title: '自定义', showNavbar: false }
+      },
+      {
+        path: 'mine',
+        name: 'Mine',
+        component: () =>
+          import(/* webpackChunkName: 'home-mine' */ '@/views/home/Mine'),
+        meta: { title: '我的', showNavbar: false }
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () =>
+      import(/* webpackChunkName: "login" */ '@/views/login/Login'),
+    meta: { title: '登录', showNavbar: false },
+    beforeEnter: (to, from, next) => {
+      const { token } = store.state.userModule
+      token ? next('/') : next()
+    }
+  },
+  {
+    path: '/virtualList',
+    name: 'VirtualList',
+    component: () =>
+      import(/* webpackChunkName: 'virtualList' */ '@/views/virtualList'),
+    meta: { title: '虚拟长列表' }
+  },
+  ...demoRoutes
 ]
 
 const router = createRouter({
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="index">
-    <div class="index-title">首页页面</div>
+    <div class="index-title">
+      首页页面
+      <div
+        class="index-title-btn"
+        @click="virtualListClick"
+      >
+        虚拟列表
+      </div>
+    </div>
     <div class="index-content">
       <div
         v-for="(item, index) in list"
@@ -22,16 +30,22 @@
 
 <script setup>
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 import { LIST_DATA } from '@/constant/demoData'
 import { getUserInfo } from '@/api/modules/test'
 
 const list = LIST_DATA
 const store = useStore()
+const router = useRouter()
 
 getUserInfo({ token: 'admin-token' }).then(res => {
   const userInfo = res.data
   store.commit('userModule/setUserInfo', userInfo)
 })
+
+const virtualListClick = () => {
+  router.push('/virtualList')
+}
 </script>
 
 <style lang="scss" scoped>
@@ -41,8 +55,15 @@ getUserInfo({ token: 'admin-token' }).then(res => {
     background: $main-bg-color;
     overflow: auto;
     &-title {
+      @include flex-row;
+      @include flex-align-center;
       font-size: 18px;
       font-weight: bold;
+      &-btn {
+        margin-left: 20px;
+        font-size: 12px;
+        color: #4685FF;
+      }
     }
     &-content {
       &-item {
