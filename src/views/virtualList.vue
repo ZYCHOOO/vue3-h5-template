d416ebf4654de7961eca367ba11275aae9858a53
曾逸超 <zengyichao@digibms.com>
2023-03-16 14:29:04 +0800

    feat(virtualList): 虚拟长列表页面更新

---
+++ src/views/virtualList.vue
@@ -0,0 +1,35 @@
+<template>
+  <virtual-list
+    :list-data="listData"
+    :start-index="0"
+    :end-index="10"
+    :item-height="20"
+    :show-num="10"
+  >
+    <template v-slot="{ itemInfo }">
+      <div>{{ itemInfo }}</div>
+    </template>
+  </virtual-list>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import VirtualList from '@/components/VirtualList/index'
+
+const listData = ref(null)
+
+const getListData = () => {
+  const result = []
+  for (let i = 0; i <= 1000; i++) {
+    result.push({ id: i, name: `列表项${i}` })
+  }
+  listData.value = result
+}
+
+getListData()
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
